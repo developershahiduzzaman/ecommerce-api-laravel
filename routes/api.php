f9cd4c84b2513177645205b63c13dc1f1926fef7
Shahiduzzaman Shahin <Shahin@IT3541.Aarongcs.com>
2026-01-11 19:49:46 +0600

    Day 1: Project setup, database design & migrations

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+// Auth
+// POST /register
+// POST /login
+
+// Category
+// GET /categories
+
+// Product
+// GET /products
+// GET /products/{id}
+
+// Order
+// POST /order
+// GET /orders
