7bd87fdd007dec779841021d125e68f1d355bba0
Shahiduzzaman Shahin <Shahin@IT3541.Aarongcs.com>
2026-01-12 17:18:35 +0600

    Day 2: Implemented user registration, login, logout & Sanctum auth

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email'=> $request->email,
+            'password'=> Hash::make($request->password),
+        ]);
+
+        return response()->json(['message'=>'User registered successfully', 'user'=>$user], 201);
+    }
+
+    // User Login
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email'=>'required|email',
+            'password'=>'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.']
+            ]);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token'=>$token,
+            'token_type'=>'Bearer',
+            'user'=>$user
+        ]);
+    }
+
+    // Logout
+    public function logout(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $request->user()->tokens()->delete();
+        return response()->json(['message'=>'Logged out successfully']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,16 @@
 // Order
 // POST /order
 // GET /orders
+
+use App\Http\Controllers\API\AuthController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/logout', [AuthController::class,'logout']);
+});
+
